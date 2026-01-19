082fe5872a1d3c14d91fb38b8004eceb9e953c41
Haoming <haoming.zhang@rwth-aachen.de>
2026-01-19 21:40:25 +0100

    fix string find bug

--- motion_capture_tracking/launch/saberguide.launch.py
+++ motion_capture_tracking/launch/saberguide.launch.py
@@ -33,11 +33,11 @@ def generate_launch_description():
                 output="screen",
                 parameters=[node_config],
             ),
-            Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
-                arguments=["-d", rviz_config],
-            ),
+         #   Node(
+         #       package="rviz2",
+         #       executable="rviz2",
+        #        name="rviz2",
+          #      arguments=["-d", rviz_config],
+          #  ),
         ]
     )
